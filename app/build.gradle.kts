5cb0991d96e44e67f1849cc35c901aa567bfcba4
anonymity060321 <anonymity060321@gmail.com>
2026-07-18 00:50:01 +0800

    Bump Bitwarden-aligned provider test version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.yiran.cerberus"
         minSdk = 36
         targetSdk = 36
-        versionCode = 30
-        versionName = "1.6.0-alpha2"
+        versionCode = 31
+        versionName = "1.6.0-alpha3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
